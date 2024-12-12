6beba16489aa3bef935afe6015853ba2246a0aaa
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-12 18:00:40 +0100

    add cors

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using webapi.core.IFeaturModule;
 using webapi.core.ioc;
 using webapi.infraestructura;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -13,13 +15,23 @@ builder.Services.AddDbContext<PizzaDbContext>(options=>{
     options.UseInMemoryDatabase("pizzas");
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                            .WithMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                            .SetPreflightMaxAge(TimeSpan.FromSeconds(2520));                          
+                      });
+});
 
 
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.MapFeatures();
 app.MapControllers();
 app.Run();
